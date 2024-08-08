e8f0c6578a666f614fc9c12bec81d22e45fe0a6d
Duc212 <duc271103@gmail.com>
2024-08-08 14:18:23 +0700

    first commit

---
+++ B_BUS/Services/NhanVienService.cs
@@ -0,0 +1,141 @@
+﻿using Aa_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_BUS.Services
+{
+    public class NhanVienService
+    {
+        AppDBContext _context;
+
+        public NhanVienService()
+        {
+            _context = new AppDBContext();
+        }
+        public string Login(string taikhoan, string matkhau)
+        {
+            var nhanvien = _context.Nhanviens.FirstOrDefault(p=> p.TaiKhoan == taikhoan && p.MatKhau == matkhau );
+            if( nhanvien == null )
+            {
+                return "";
+            }else
+            {
+                return nhanvien.Vaitro  + ":" + nhanvien.TaiKhoan;
+            }
+        }
+        public string CreateNV(string taikhoan, string matkhau, int vaitro)
+        {
+            Nhanvien nhanvien = new Nhanvien()
+            {
+                Vaitro = vaitro,
+                TaiKhoan = taikhoan,
+                MatKhau = matkhau,
+            };
+            try
+            {
+                _context.Nhanviens.Add(nhanvien);
+                _context.SaveChanges();
+                return "Tạo Thành Công";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message + ex.InnerException; 
+                
+            }
+            
+        }
+        public List<Nhanvien> GetNhanViens()
+        {
+            return _context.Nhanviens.ToList();
+        }
+        public string XoaNV(string taikhoan)
+        {
+            var deleteItem = _context.Nhanviens.Find(taikhoan);
+
+            try
+            {
+                _context.Nhanviens.Remove(deleteItem);
+                _context.SaveChanges();
+                return "Xóa Thành Công";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        public string KhoaNV(string taikhoan)
+        {
+            var nhanvien = _context.Nhanviens.Find(taikhoan);
+            if(nhanvien.Vaitro == 1)
+            {
+                return "Không được khóa tài khoản admin";
+            }
+            else if(nhanvien.Vaitro == 0)
+            {
+                return "Tài Khoản đã bị Khóa ";
+            }
+            nhanvien.Vaitro = 0;
+
+            try
+            {
+                _context.Nhanviens.Update(nhanvien);
+                _context.SaveChanges(); return "Tài Khoản đã bị khóa " + taikhoan;
+            }
+            catch (Exception ex)
+            {
+
+                return "Khóa Thất Bại" + ex.Message + ex.InnerException;
+            }
+           
+        }
+        public string MoKhoaNV(string taikhoan)
+        {
+            var nhanvien = _context.Nhanviens.Find(taikhoan);
+            
+            if(nhanvien.Vaitro != 0)
+            {
+                return "Tài Khoản đang hoạt động";
+            }
+            nhanvien.Vaitro = 2;
+            try
+            {
+                _context.Nhanviens.Update(nhanvien);
+                _context.SaveChanges(); return "Đã Mở Khóa Tài Khoản  " + taikhoan;
+            }
+            catch (Exception ex)
+            {
+
+                return "Mở Khóa Thất Bại" + ex.Message + ex.InnerException;
+            }
+
+        }
+
+        public string UpdateNV(string taikhoan, string matkhau, int vaitro)
+        {
+            var nhanvien = _context.Nhanviens.Find(taikhoan);
+            if(nhanvien == null) 
+            {
+                return "Không tìm thấy tài khoản cần sửa ";
+            }else
+            {
+                nhanvien.MatKhau = matkhau;
+                nhanvien.Vaitro = vaitro;
+                try
+                {
+                    _context.Nhanviens.Update(nhanvien);
+                    _context.SaveChanges();
+                    return "Sửa Thành Công";
+                }
+                catch (Exception ex)
+                {
+
+                    return "Sửa Thất Bại"+ ex.Message + ex.InnerException;
+                }
+            }
+        }
+    }
+}
---
+++ B_BUS/Services/SanPhamServices.cs
@@ -0,0 +1,78 @@
+﻿using Aa_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_BUS.Services
+{
+    public class SanPhamServices
+    {
+        AppDBContext _context;
+        public SanPhamServices()
+        {
+            _context = new AppDBContext();
+        }
+        public List<SanPham> GetAllSanPhams() 
+        {
+            return _context.SanPhams.ToList();
+        }
+        public string TaoSanPham(SanPham sanPham)
+        {
+            try
+            {
+                _context.SanPhams.Add(sanPham);
+                _context.SaveChanges();
+                return "Thêm sản phẩm thành công";
+            }
+            catch (Exception ex)
+            {
+                return "Them That Bai" + ex.Message;
+            }
+        }
+        public List<SanPham> TimTheoTen(string tensanpham)
+        {
+            return _context.SanPhams.Where
+                (p=> p.TenSanPham.ToLower().Contains(tensanpham.ToLower())).ToList();
+
+        }
+        public string KhoaSP(Guid id)
+        {
+            var sanpham = _context.SanPhams.Find(id);
+            if (sanpham.TrangThai == 0) return "Sản Phẩm Không bị Khóa ,Không thể mở khóa";
+            else
+            {
+                sanpham.TrangThai  = 0;
+                _context.SaveChanges();
+                return "Khoa San Pham Thanh Cong";
+            }
+        }
+        public string MoKhoaSanPham(Guid id)
+        {
+            var sanpham = _context.SanPhams.Find(id);
+            if (sanpham.TrangThai != 0) return "Sản Phẩm Không bị Khóa ,Không thể mở khóa";
+            else
+            {
+                sanpham.TrangThai = 1; 
+                _context.SaveChanges();
+                return "Khoa San Pham Thanh Cong";
+            }
+        }
+        public string UpdateSanPham(SanPham sanpham)
+        {
+            var updateSanPham = _context.SanPhams.Find(sanpham.Id);
+            updateSanPham.TenSanPham = sanpham.TenSanPham;
+            updateSanPham.ImgURL = sanpham.ImgURL;
+            updateSanPham.TrangThai = sanpham.TrangThai;
+            updateSanPham.Gia = sanpham.Gia;
+            updateSanPham.SoLuongTon = sanpham.SoLuongTon;
+            updateSanPham.NhaCungCap = sanpham.NhaCungCap;
+
+            _context.SanPhams.Update(updateSanPham);
+            _context.SaveChanges();
+            return "Sua Thanh Cong";
+        }
+       
+    }
+}
